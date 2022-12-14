60b91482da1b0650eed2e3ecd407812bc6a41575
Alexander Steos <alexandr@steos.io>
2022-12-14 06:52:46 +0300

    Доделал сетку, добавил контекстное меню, реализовал добавление базовых виджетов

---
+++ scripts/contextMenu.js
@@ -0,0 +1,16 @@
+const contextMenu = document.getElementById("contextMenu")
+
+grid.addEventListener("click", (event) => {
+   if (event.composedPath().find((tag) => tag?.classList?.contains("grid_element") ?? false)) {
+         const x = event.clientX
+         const y = event.clientY
+
+         contextMenu.style.display = "block"
+         contextMenu.style.top = `${y + 310 > grid.clientHeight ? y - 330 : y - 10}px`
+         contextMenu.style.left = `${x + 240 > grid.clientWidth ? x - 240 : x + 20}px`
+
+         gridElement = event.composedPath().find((tag) => tag.classList.contains("grid_element"))
+   } else {
+      contextMenu.style.display = "none"
+   }
+})
---
+++ scripts/widgets.js
@@ -0,0 +1,23 @@
+function addChat() {
+   gridElement.innerHTML = "<div>Чат</div>"
+   gridElement.style.opacity = 1
+   contextMenu.style.display = "none"
+}
+
+function addCalendar() {
+   gridElement.innerHTML = "<div>Календарь</div>"
+   gridElement.style.opacity = 1
+   contextMenu.style.display = "none"
+}
+
+function addChart() {
+   gridElement.innerHTML = "<div>График</div>"
+   gridElement.style.opacity = 1
+   contextMenu.style.display = "none"
+}
+
+function addMail() {
+   gridElement.innerHTML = "<div>Почта</div>"
+   gridElement.style.opacity = 1
+   contextMenu.style.display = "none"
+}
