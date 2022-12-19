e87395a8904019d679d94f300e4865d6ba301312
Alexander Steos <alexandr@steos.io>
2022-12-19 06:58:33 +0300

    Добавил расширяемость виджетов в ширину при добавлении

--- scripts/grid.js
+++ scripts/grid.js
@@ -2,21 +2,23 @@ const grid = document.getElementById("grid")
 let gridElement
 
 function generateGrid() {
-   const gridWidth = grid.clientWidth
-   const gridHeight = grid.clientHeight
-   const numberElementsInRow = Math.floor(gridWidth / (200 + 25))
-   const numberElementsInColumn = Math.floor(gridHeight / (200 + 25))
+   const numberElementsInRow = Math.floor(document.body.clientWidth / 225)
+   const numberElementsInColumn = Math.floor(document.body.clientHeight / 225)
 
    let divs = []
+   let index = 0
+
    for (let i = 0; i < numberElementsInColumn; i++) {
       const row = []
       for (let j = 0; j < numberElementsInRow; j++) {
-         row.push(`<div class="grid_element empty_element">
-                        <a class="uk-icon-button uk-icon" uk-icon="plus"></a>
-                  </div>`)
+         row.push(`<div id="${ index }" class="grid_element empty_element">
+         <a class="uk-icon-button uk-icon" uk-icon="plus"></a>
+   </div>`)
+         index++
       }
       divs.push(`<div class="grid_row">${row.join("")}</div>`)
    }
+
    grid.innerHTML = divs.join("")
    addDragAndDropEvents()
 }
--- scripts/widgets.js
+++ scripts/widgets.js
@@ -1,31 +1,65 @@
-function addChat() {
+function addWidget(widget, cols = 1, rows = 1) {
+   switch(widget) {
+      case "chat":
+         gridElement.innerHTML = "<div>Чат</div>"
+         break
+      case "calendar":
+         gridElement.innerHTML = "<div>Календарь</div>"
+         break
+      case "chart":
+         gridElement.innerHTML = "<div>График</div>"
+         break
+      case "mail":
+         gridElement.innerHTML = "<div>Почта</div>"
+         break
+      case "statistic":
+         gridElement.innerHTML = "<div>Статистика</div>"
+         break
+   }
+
+   if (rows > 1) {
+      expandWidgetInRow(rows)
+   }
+   if (cols > 1) {
+      expandWidgetInColumn(cols)
+   }
+
    gridElement.classList.remove("empty_element")
    gridElement.classList.add("widget")
-   gridElement.innerHTML = "<div>Чат</div>"
    contextMenu.style.display = "none"
    addDragAndDropEvents()
 }
 
-function addCalendar() {
-   gridElement.classList.remove("empty_element")
-   gridElement.classList.add("widget")
-   gridElement.innerHTML = "<div>Календарь</div>"
-   contextMenu.style.display = "none"
-   addDragAndDropEvents()
+function expandWidgetInRow(rows) {
+   const gridElements = document.querySelectorAll(".grid_element")
+   const gridElementIndex = +gridElement.getAttribute("id")
+   const numberElementsInRow = Math.floor(document.body.clientWidth / 225)
+
+   if (Math.floor(gridElementIndex / numberElementsInRow) < Math.floor((gridElementIndex + rows - 1) / numberElementsInRow)) {
+      for (let i = 1; i < rows; i++) {
+         gridElements[gridElementIndex - i].style.display = "none"
+      }
+   } else {
+      for (let i = 1; i < rows; i++) {
+         gridElements[gridElementIndex + i].style.display = "none"
+      }
+   }
+   gridElement.style.width = `${ 200 * rows + 25 * (rows - 1) }px`
 }
 
-function addChart() {
-   gridElement.classList.remove("empty_element")
-   gridElement.classList.add("widget")
-   gridElement.innerHTML = "<div>График</div>"
-   contextMenu.style.display = "none"
-   addDragAndDropEvents()
-}
+function expandWidgetInColumn(cols) {
+   const gridElements = document.querySelectorAll(".grid_element")
+   const gridElementIndex = +gridElement.getAttribute("id")
+   const numberElementsInRow = Math.floor(document.body.clientWidth / 225)
 
-function addMail() {
-   gridElement.classList.remove("empty_element")
-   gridElement.classList.add("widget")
-   gridElement.innerHTML = "<div>Почта</div>"
-   contextMenu.style.display = "none"
-   addDragAndDropEvents()
+   if (gridElementIndex + numberElementsInRow * cols - 1 > gridElements.length) {
+      for (let i = 1; i < cols; i++) {
+         gridElements[gridElementIndex - numberElementsInRow * i].style.display = "none"
+      }
+   } else {
+      for (let i = 1; i < cols; i++) {
+         console.log(gridElementIndex + numberElementsInRow * i)
+         gridElements[gridElementIndex + numberElementsInRow * i].style.display = "none"
+      }
+   }
 }
