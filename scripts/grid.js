a94d76026d6b16528aeb7be5d5cae12328adfd0c
Alexander Steos <alexandr@steos.io>
2022-12-21 13:51:18 +0300

    Доделал грид сетку

--- scripts/grid.js
+++ scripts/grid.js
@@ -2,6 +2,8 @@ const grid = document.getElementById("grid")
 let gridElement
 let gridElementIndex = 0
 let gridLayout = []
+let gridElementsLayout = []
+let gridEmptyLayout = []
 
 const numberElementsInRow = Math.floor(document.body.clientWidth / 225)
 const numberElementsInColumn = Math.floor(document.body.clientHeight / 225)
@@ -12,21 +14,24 @@ function generateGrid() {
 
    for (let i = 0; i < numberElementsInColumn; i++) {
       const layoutRow = []
+      const elementsRow = []
       for (let j = 0; j < numberElementsInRow; j++) {
-         divs.push(`<div id="${ index }" class="grid_element empty_element" style="grid-area: empty-${index}">
+         const newDiv = `<div id="${ index }" class="grid_element empty_element" style="grid-area: empty-${index}">
          <a class="uk-icon-button uk-icon" uk-icon="plus"></a>
-         </div>`)
+         </div>`
+         divs.push(newDiv)
+         elementsRow.push(newDiv)
+         gridEmptyLayout.push(`empty-${index}`)
          layoutRow.push(`empty-${index}`)
          index++
       }
 
-      gridLayout.push(layoutRow.join(" "))
+      gridElementsLayout.push(elementsRow)
+      gridLayout.push(layoutRow)
    }
 
-   console.log(gridLayout)
-
    grid.innerHTML = divs.join("")
-   grid.style.gridTemplateAreas = `"empty-0 empty-0 empty-2 empty-3" "empty-0 empty-0 empty-6 empty-3" "empty-8 empty-9 empty-10 empty-3"`
+   grid.style.gridTemplateAreas = `'${gridLayout.map((row) => row.join(" ")).join("' '")}'`
    addDragAndDropEvents()
 }
 generateGrid()
